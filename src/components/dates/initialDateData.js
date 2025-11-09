8c3c6c7e64c7853290b2a143cc521aa471eb9171
“Holly <hollyefig@gmail.com>
2025-11-08 20:36:48 -0500

    photos added

--- src/components/dates/initialDateData.js
+++ src/components/dates/initialDateData.js
@@ -1,179 +1,203 @@
 import day1 from "../../IMGs/date-imgs/day-1.png";
 import photo1 from "../../IMGs/photos/photo-1.jpg";
 import day2 from "../../IMGs/date-imgs/day-2.png";
+import photo2 from "../../IMGs/photos/photo-2.jpg";
 import day3 from "../../IMGs/date-imgs/day-3.png";
+import photo3 from "../../IMGs/photos/photo-3.jpg";
 import day4 from "../../IMGs/date-imgs/day-4.png";
+import photo4 from "../../IMGs/photos/photo-4.jpg";
 import day5 from "../../IMGs/date-imgs/day-5.png";
+import photo5 from "../../IMGs/photos/photo-5.jpg";
 import day6 from "../../IMGs/date-imgs/day-6.png";
+import photo6 from "../../IMGs/photos/photo-6.jpg";
 import day7 from "../../IMGs/date-imgs/day-7.png";
+import photo7 from "../../IMGs/photos/photo-7.jpg";
 import day8 from "../../IMGs/date-imgs/day-8.png";
+import photo8 from "../../IMGs/photos/photo-8.jpg";
 import day9 from "../../IMGs/date-imgs/day-9.png";
+import photo9 from "../../IMGs/photos/photo-9.jpg";
 import day10 from "../../IMGs/date-imgs/day-10.png";
+import photo10 from "../../IMGs/photos/photo-10.jpg";
 import day11 from "../../IMGs/date-imgs/day-11.png";
+import photo11 from "../../IMGs/photos/photo-11.jpg";
 import day12 from "../../IMGs/date-imgs/day-12.png";
+import photo12 from "../../IMGs/photos/photo-12.jpg";
 import day13 from "../../IMGs/date-imgs/day-13.png";
+import photo13 from "../../IMGs/photos/photo-13.jpg";
 import day14 from "../../IMGs/date-imgs/day-14.png";
+import photo14 from "../../IMGs/photos/photo-14.jpg";
 import day15 from "../../IMGs/date-imgs/day-15.png";
+import photo15 from "../../IMGs/photos/photo-15.jpg";
 import day16 from "../../IMGs/date-imgs/day-16.png";
+import photo16 from "../../IMGs/photos/photo-16.jpg";
 import day17 from "../../IMGs/date-imgs/day-17.png";
+import photo17 from "../../IMGs/photos/photo-17.jpg";
 import day18 from "../../IMGs/date-imgs/day-18.png";
+import photo18 from "../../IMGs/photos/photo-18.jpg";
 import day19 from "../../IMGs/date-imgs/day-19.png";
+import photo19 from "../../IMGs/photos/photo-19.jpg";
 import day20 from "../../IMGs/date-imgs/day-20.png";
+import photo20 from "../../IMGs/photos/photo-20.jpg";
 import day21 from "../../IMGs/date-imgs/day-21.png";
+import photo21 from "../../IMGs/photos/photo-21.jpg";
 import day22 from "../../IMGs/date-imgs/day-22.png";
+import photo22 from "../../IMGs/photos/photo-22.jpg";
 import day23 from "../../IMGs/date-imgs/day-23.png";
+import photo23 from "../../IMGs/photos/photo-23.jpg";
 import day24 from "../../IMGs/date-imgs/day-24.png";
+import photo24 from "../../IMGs/photos/photo-24.jpg";
 import day25 from "../../IMGs/date-imgs/day-25.png";
+import photo25 from "../../IMGs/photos/photo-25.jpg";
 
 export const dateData = [
   {
     calImg: day1,
     text: "WOOOORDDSSS!!!",
-    expire: new Date("November 2, 2025 00:00:00"),
+    expire: new Date("October 2, 2025 00:00:00"),
     photo: photo1,
   },
   {
     calImg: day2,
     text: "WOOOORDDSSS!!!",
-    expire: new Date("November 3, 2025 00:00:00"),
-    photo: photo1,
+    expire: new Date("October 3, 2025 00:00:00"),
+    photo: photo2,
   },
   {
     calImg: day3,
     text: "WOOOORDDSSS!!!",
-    expire: new Date("November 4, 2025 00:00:00"),
-    photo: photo1,
+    expire: new Date("October 4, 2025 00:00:00"),
+    photo: photo3,
   },
   {
     calImg: day4,
     text: "WOOOORDDSSS!!!",
-    expire: new Date("November 5, 2025 00:00:00"),
-    photo: photo1,
+    expire: new Date("October 5, 2025 00:00:00"),
+    photo: photo4,
   },
   {
     calImg: day5,
     text: "WOOOORDDSSS!!!",
-    expire: new Date("November 6, 2025 00:00:00"),
-    photo: photo1,
+    expire: new Date("October 6, 2025 00:00:00"),
+    photo: photo5,
   },
   {
     calImg: day6,
     text: "WOOOORDDSSS",
-    expire: new Date("November 7, 2025 00:00:00"),
-    photo: photo1,
+    expire: new Date("October 7, 2025 00:00:00"),
+    photo: photo6,
   },
   {
     calImg: day7,
     text: "WOOOORDDSSS!!!",
-    expire: new Date("November 8, 2025 00:00:00"),
-    photo: photo1,
+    expire: new Date("October 8, 2025 00:00:00"),
+    photo: photo7,
   },
   {
     calImg: day8,
     text: "WOOOORDDSSS!!!",
-    expire: new Date("November 9, 2025 00:00:00"),
-    photo: photo1,
+    expire: new Date("October 9, 2025 00:00:00"),
+    photo: photo8,
   },
   {
     calImg: day9,
     text: "WOOOORDDSSS!!!",
-    expire: new Date("November 10, 2025 00:00:00"),
-    photo: photo1,
+    expire: new Date("October 10, 2025 00:00:00"),
+    photo: photo9,
   },
   {
     calImg: day10,
     text: "WOOOORDDSSS!!!",
-    expire: new Date("November 11, 2025 00:00:00"),
-    photo: photo1,
+    expire: new Date("October 11, 2025 00:00:00"),
+    photo: photo10,
   },
   {
     calImg: day11,
     text: "WOOOORDDSSS!!!",
-    expire: new Date("November 12, 2025 00:00:00"),
-    photo: photo1,
+    expire: new Date("October 12, 2025 00:00:00"),
+    photo: photo11,
   },
   {
     calImg: day12,
     text: "WOOOORDDSSS!!!",
-    expire: new Date("November 13, 2025 00:00:00"),
-    photo: photo1,
+    expire: new Date("October 13, 2025 00:00:00"),
+    photo: photo12,
   },
   {
     calImg: day13,
     text: "WOOOORDDSSS!!!",
-    expire: new Date("November 14, 2025 00:00:00"),
-    photo: photo1,
+    expire: new Date("October 14, 2025 00:00:00"),
+    photo: photo13,
   },
   {
     calImg: day14,
     text: "WOOOORDDSSS!!!",
-    expire: new Date("November 15, 2025 00:00:00"),
-    photo: photo1,
+    expire: new Date("October 15, 2025 00:00:00"),
+    photo: photo14,
   },
   {
     calImg: day15,
     text: "WOOOORDDSSS!!!",
-    expire: new Date("November 16, 2025 00:00:00"),
-    photo: photo1,
+    expire: new Date("October 16, 2025 00:00:00"),
+    photo: photo15,
   },
   {
     calImg: day16,
     text: "WOOOORDDSSS!!!",
-    expire: new Date("November 17, 2025 00:00:00"),
-    photo: photo1,
+    expire: new Date("October 17, 2025 00:00:00"),
+    photo: photo16,
   },
   {
     calImg: day17,
     text: "WOOOORDDSSS!!!",
-    expire: new Date("November 18, 2025 00:00:00"),
-    photo: photo1,
+    expire: new Date("October 18, 2025 00:00:00"),
+    photo: photo17,
   },
   {
     calImg: day18,
     text: "WOOOORDDSSS!!!",
-    expire: new Date("November 19, 2025 00:00:00"),
-    photo: photo1,
+    expire: new Date("October 19, 2025 00:00:00"),
+    photo: photo18,
   },
   {
     calImg: day19,
     text: "WOOOORDDSSS!!!",
-    expire: new Date("November 20, 2025 00:00:00"),
-    photo: photo1,
+    expire: new Date("October 20, 2025 00:00:00"),
+    photo: photo19,
   },
   {
     calImg: day20,
     text: "WOOOORDDSSS!!!",
-    expire: new Date("November 21, 2025 00:00:00"),
-    photo: photo1,
+    expire: new Date("October 21, 2025 00:00:00"),
+    photo: photo20,
   },
   {
     calImg: day21,
     text: "WOOOORDDSSS!!!",
-    expire: new Date("November 22, 2025 00:00:00"),
-    photo: photo1,
+    expire: new Date("October 22, 2025 00:00:00"),
+    photo: photo21,
   },
   {
     calImg: day22,
     text: "WOOOORDDSSS!!!",
-    expire: new Date("November 23, 2025 00:00:00"),
-    photo: photo1,
+    expire: new Date("October 23, 2025 00:00:00"),
+    photo: photo22,
   },
   {
     calImg: day23,
     text: "WOOOORDDSSS!!!",
-    expire: new Date("November 24, 2025 00:00:00"),
-    photo: photo1,
+    expire: new Date("October 24, 2025 00:00:00"),
+    photo: photo23,
   },
   {
     calImg: day24,
     text: "WOOOORDDSSS!!!",
-    expire: new Date("November 25, 2025 00:00:00"),
-    photo: photo1,
+    expire: new Date("October 25, 2025 00:00:00"),
+    photo: photo24,
   },
   {
     calImg: day25,
     text: "WOOOORDDSSS!!!",
-    expire: new Date("November 26, 2025 00:00:00"),
-    photo: photo1,
+    expire: new Date("October 26, 2025 00:00:00"),
+    photo: photo25,
   },
 ];
